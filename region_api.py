6ffa1c76a400aa16311f053ee1fec23a4a84c1a0
Kokitis <sypilus@gmail.com>
2017-05-31 13:47:47 -0400

    Added additional equivilant codes.

--- region_api.py
+++ region_api.py
@@ -277,8 +277,15 @@ def testDataset():
 
 EQUIVILANT_CODES = {
 	'population': {
+		'Historical Country Profiles': 'POP'
 		'World Development Indicators': 'SP.POP.TOTL',
 		'World Economic Outlook': 'LP'
+	},
+	'gross domestic product': {
+		'Historical Country Profiles': 'GDP'
+	}
+	'gross domestic product per capita': {
+		'Historical Country Profiles': 'GDPPC'
 	}
 }
 
