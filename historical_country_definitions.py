490f90e80d1c1bb565b215fd7099f5c126e6868f
Kokitis <sypilus@gmail.com>
2017-06-04 18:21:47 -0400

    Changed historical definitions to a .py file

---
+++ historical_country_definitions.py
@@ -0,0 +1,54 @@
+[
+	{
+		"baseCode": "GBR",
+		"description": "",
+		"name": "United Kingdom of Great Britain and Ireland",
+		"definition":{
+			"GBR:1900": ["GBR", "IRL"]
+		}
+	},
+	{
+		"basecode": "EU",
+		"description": "",
+		"name": "European Union",
+		"definition": {
+			"EU:1958": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU"],
+			
+			"EU:1973": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR"],
+			
+			"EU:1981": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR", "GRC"],
+			
+			"EU:1986": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR", "GRC", "PRT", "ESP"],
+			
+			"EU:1995": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR", "GRC", "PRT", "ESP", 
+						"AUT", "FIN", "SWE"],
+			"EU:2004": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR", "GRC", "PRT", "ESP", 
+						"AUT", "FIN", "SWE", "HUN", "CYP", "CZE", "EST", "LVA", "LTU", "MLT", "POL", "SVK",
+						"SVN"],
+			"EU:2007": ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR", "GRC", "PRT", "ESP", 
+						"AUT", "FIN", "SWE", "HUN", "CYP", "CZE", "EST", "LVA", "LTU", "MLT", "POL", "SVK",
+						"SVN", "BGR", "ROU"],
+			"EU:2013":  ["BEL", "FRA", "ITA", "LUX", "NLD", "DEU", "DNK", "IRL", "GBR", "GRC", "PRT", "ESP", 
+						"AUT", "FIN", "SWE", "HUN", "CYP", "CZE", "EST", "LVA", "LTU", "MLT", "POL", "SVK",
+						"SVN", "BGR", "ROU", "HRV"],
+		}
+	},
+	{
+		"basecode": "ROME",
+		"description": "",
+		"definition": {
+		},
+		"name": "Roman Empire",
+		"timeline": {
+			"from": "27 BC",
+			"to":   "1453 AD",
+			"max":  "117 AD"
+		} 
+	},
+	{
+		"basecode": "USSR",
+		"description": "",
+		"name": "Soviet Union",
+		"definition": []
+	}
+]
